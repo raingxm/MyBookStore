cf490c14e9be506906dde13054f7b8c34ce3e470
zhangxu <xuzhang@thoughtworks.com>
2015-05-11 20:38:36 +0800

    refactor code, change home controller name

---
+++ test/com/springapp/mvc/HomeControllerTest.java
@@ -0,0 +1,11 @@
+package com.springapp.mvc;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+    @Test
+    public void testHelloPage() throws Exception {
+        HomeController homeController = new HomeController();
+
+    }
+}
