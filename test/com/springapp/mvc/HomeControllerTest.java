f3054549f273497a699d92cca5e131f1e3f5c066
zhangxu <xuzhang@thoughtworks.com>
2015-05-13 07:50:56 +0800

    add unit test to home controller

--- test/com/springapp/mvc/HomeControllerTest.java
+++ test/com/springapp/mvc/HomeControllerTest.java
@@ -2,9 +2,13 @@ package com.springapp.mvc;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class HomeControllerTest {
     @Test
     public void testHelloPage() throws Exception {
         HomeController homeController = new HomeController();
+        assertThat(homeController.homePage(), is("home"));
     }
 }
