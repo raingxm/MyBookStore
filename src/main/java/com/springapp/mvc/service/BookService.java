aa63fd12aba3e9730eea3ce90e60902095dcc3b1
raingxm <raingxm@163.com>
2015-06-03 20:56:15 +0800

    add properties file

--- src/main/java/com/springapp/mvc/service/BookService.java
+++ src/main/java/com/springapp/mvc/service/BookService.java
@@ -3,6 +3,8 @@ package com.springapp.mvc.service;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.springapp.mvc.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,9 +17,17 @@ import java.util.List;
 
 @Service
 public class BookService {
-    private RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+    private final String dataSource;
+
+    @Autowired
+    public BookService(RestTemplate restTemplate, @Value("${book.data.source}") String dataSource) {
+        this.restTemplate = restTemplate;
+        this.dataSource = dataSource;
+    }
+
     public List<Book> getBooks() {
-        UriComponents uri = UriComponentsBuilder.fromHttpUrl("http://localhost:3000").build();
+        UriComponents uri = UriComponentsBuilder.fromHttpUrl(dataSource).build();
         try {
             URI req = uri.encode().toUri();
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(req, String.class);
