1a8b74d1b5fe15bd6d43396bee14878acf7bc15a
raingxm <raingxm@163.com>
2015-06-15 08:33:54 +0800

    insert order to mongodb, quick fix

---
+++ src/main/java/com/springapp/mvc/service/OrderService.java
@@ -0,0 +1,22 @@
+package com.springapp.mvc.service;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.stereotype.Service;
+import com.springapp.mvc.model.User;
+
+
+@Service
+public class OrderService {
+    public void saveOrder(User user) throws Exception {
+        MongoClient client = new MongoClient();
+        MongoDatabase db = client.getDatabase("store");
+        MongoCollection collection = db.getCollection("order");
+        Document insertUser = new Document().append("name", user.getName()).
+                append("phone", user.getPhone());
+        Document order = new Document().append("name", "C plus").append("user", insertUser);
+        collection.insertOne(order);
+    }
+}
--- src/main/java/com/springapp/mvc/web/OrderController.java
+++ src/main/java/com/springapp/mvc/web/OrderController.java
@@ -1,6 +1,8 @@
 package com.springapp.mvc.web;
 
 import com.springapp.mvc.model.User;
+import com.springapp.mvc.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/saveOrder")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
     @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView saveUserInfo(@ModelAttribute User user) {
+    public ModelAndView saveUserInfo(@ModelAttribute User user) throws Exception {
         ModelAndView modelAndView = new ModelAndView("cart");
+        orderService.saveOrder(user);
         modelAndView.addObject("user", user);
         return modelAndView;
     }
