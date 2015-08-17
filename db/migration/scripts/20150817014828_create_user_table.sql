fb9d81a32420ddabf020fd3414354ff8213bbda7
raingxm <raingxm@163.com>
2015-08-17 19:03:02 +0800

    add create user table migration scripts

--- db/migration/scripts/20150817014828_create_user_table.sql
+++ db/migration/scripts/20150817014828_create_user_table.sql
@@ -1,9 +1,14 @@
 -- // create user table
 -- Migration SQL that makes the change goes here.
 
-
+CREATE TABLE USER (
+  user_id SERIAL PRIMARY KEY,
+  user_name VARCHAR(255) NOT NULL UNIQUE,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
-
+DROP TABLE USER;
 
