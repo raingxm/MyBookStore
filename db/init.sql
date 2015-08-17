cf20eb2301112400268e7bb4443a4b675c5f21d6
raingxm <raingxm@163.com>
2015-08-17 19:04:13 +0800

    add init database data

---
+++ db/init.sql
@@ -0,0 +1,4 @@
+-- init database, insert data to table
+
+INSERT INTO USER VALUES(1, 'raingxm', 'runingman@gmail.com', 'fc5e038d38a57032085441e7fe7010b0');
+INSERT INTO USER VALUES(2, 'runing', 'runing@gmail.com', '5d41402abc4b2a76b9719d911017c592');
