ecfdb81e6edddc534c4d1d821701ec72057f2c1b
raingxm <raingxm@163.com>
2015-08-24 21:43:02 +0800

    add user id for user model

--- src/main/java/com/springapp/mvc/model/User.java
+++ src/main/java/com/springapp/mvc/model/User.java
@@ -1,10 +1,23 @@
 package com.springapp.mvc.model;
 
 public class User {
+    private Long user_id;
     private String name;
     private String phone;
     private String email;
     private String address;
+
+    public User() {
+        this.user_id = 0L;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -23,7 +36,6 @@ public class User {
     }
 
     public String getName() {
-
         return name;
     }
 
