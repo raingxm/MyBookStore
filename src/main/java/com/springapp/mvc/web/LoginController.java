2fcbccc5b53258279153ed4801beacc002a49662
raingxm <raingxm@163.com>
2016-03-07 21:18:03 +0800

    let success parameter to be optional

--- src/main/java/com/springapp/mvc/web/LoginController.java
+++ src/main/java/com/springapp/mvc/web/LoginController.java
@@ -1,8 +1,6 @@
 package com.springapp.mvc.web;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class LoginController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView loginPage(@RequestParam boolean success) {
+    public ModelAndView loginPage(@RequestParam(value = "success", required = false) boolean success) {
         ModelAndView login = new ModelAndView("login");
         if(success) {
             login.getModel().put("success", true);
